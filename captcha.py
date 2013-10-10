ddedc83bea7812cb982bc638d66e7b58d5472fac
Bidon Pomoev <yoba123@yandex.ru>
2013-10-10 17:27:45 +0400

    no message

--- captcha.py
+++ captcha.py
@@ -10,10 +10,10 @@ from config import CAPTCHA_FILES
 def get_captcha_img():
     import datetime
     from flask import make_response
-    if not session.get('valid'):
+    if not session.get('valid_captcha'):
         abort(403)
 
-    response = make_response(send_from_directory('captcha', session['valid']['file']))
+    response = make_response(send_from_directory('captcha', session['valid_captcha']['file']))
 
     response.headers.add('Last-Modified', datetime.datetime.now())
     response.headers.add('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
@@ -27,18 +27,19 @@ def show_captcha():
     from flask import jsonify
     if request.args.get('c'):
         captcha_res = request.args.get('c')
-        if captcha_res and session.get('valid'):
-            session['captcha-resolve'] = (captcha_res == session.get('valid')['name'])
-            session['valid'] = None
+        if captcha_res and session.get('valid_captcha'):
+            session['captcha-resolve'] = (captcha_res == session.get('valid_captcha')['name'])
+            session['valid_captcha'] = None
         return jsonify(success = session.get('captcha-resolve'))
 
     if session.get('captcha-resolve') == True:
-        session['valid'] = None
+        session['valid_captcha'] = None
         return redirect('/index')
 
     names = list()
     for c in CAPTCHA_FILES:
         names.append(c['name'])
     shuffle(names)
-    session['valid'] = choice(CAPTCHA_FILES)
+    if not session.get('valid_captcha'):
+        session['valid_captcha'] = choice(CAPTCHA_FILES)
     return render_template('captcha.html', names = names)
