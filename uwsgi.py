8f2f5aafc85b82672e9602269f835f857f0490c6
Bidon Pomoev <yoba123@yandex.ru>
2013-10-16 23:24:20 +0400

    uwsgi server_name fix

--- uwsgi.py
+++ uwsgi.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # coding=utf-8
 from app import app
-if app.config.get('DEBUG_ENABLED'):
-	app.debug=True
-	from werkzeug.debug import DebuggedApplication
-	app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+if app.config['DEBUG_ENABLED']:
+    app.debug=True
+    from werkzeug.debug import DebuggedApplication
+    app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+    app.config['SERVER_NAME'] = app.config['DEBUG_SERVER_NAME']
+else:
+    app.config['SERVER_NAME'] = app.config['RELEASE_SERVER_NAME']
