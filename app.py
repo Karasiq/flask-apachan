eddae3f75b7199807da62ec6d887dcc90a45dce5
Bidon Pomoev <yoba123@yandex.ru>
2013-10-22 03:38:04 +0400

    better hidden boards hide

--- app.py
+++ app.py
@@ -732,7 +732,7 @@ def allsections(page=1):
 @app.route('/boards/<SectionName>/<int:page>')
 def section(SectionName, page=1):
     if app.config['SECTIONS'].get(SectionName) is None or \
-            (SectionName in app.config['HIDDEN_BOARDS'] and (not session.get('uid') or session.get('crawler') or not get_user(session['uid']).last_post or check_banned())):
+            (SectionName in app.config['HIDDEN_BOARDS'] and (not session.get('uid') or session.get('crawler') or get_user(session['uid']).last_post < datetime.now() + timedelta(hours=3) or check_banned())):
         return render_template("error.html", errortitle = u"Раздел не найден")
 
     else:
